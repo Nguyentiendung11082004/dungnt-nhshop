823f481f5c6dea8c391163ac3ffd3c61879fc719
Dung <nguyentiendung11082004@gmail.com>
2024-03-16 23:37:12 +0700

    update

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        cart: resolve(__dirname, './cart/index.html'),
+        checkout: resolve(__dirname, './checkout/index.html'),
+        detailproduct: resolve(__dirname, './detailproduct/index.html'),
+        login: resolve(__dirname, './login/index.html'),
+        shop: resolve(__dirname, './shop/index.html'),
+      },
+    },
+  },
+})
